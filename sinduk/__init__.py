2f980fe15f14a5a59636c8d9bf9222ab852d95d7
anweshadas <anwesha@das.community>
2021-04-20 11:32:42 +0530

    Adds the --init call

--- sinduk/__init__.py
+++ sinduk/__init__.py
@@ -1,12 +1,28 @@
 import os
 from os.path import basename
 import johnnycanencrypt as jce
+import argparse
 
-def init():
+
+def init(keyfile: str):
     "initials the sinduk project for the user"
-    dirname = os.path.expanduser("~/.sinduk")
-    if os.path.exists(dirname):
+    if not os.path.exists(keyfile):
+        print("The OpenPGP keyfile does not exist.")
         return
-    os.mkdir(dirname)
+    dirname = os.path.expanduser("~/.sinduk")
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+    ks = jce.KeyStore(dirname)
+    ks.import_cert(keyfile)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--init", help="pass the OpenPGP key file ", type=str)
+    args = parser.parse_args()
+    if args.init:
+        init(args.init)
+
+
+
     
     
