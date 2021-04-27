d5a8ed7736f7fadc5f079afc85815076a61898dd
anweshadas <anwesha@das.community>
2021-04-27 14:39:36 +0530

    Adds the code to show password

--- sinduk/__init__.py
+++ sinduk/__init__.py
@@ -2,7 +2,10 @@ import os
 from os.path import basename
 import johnnycanencrypt as jce
 import argparse
+import sys
 import getpass
+import pathlib
+from typing import List
 
 
 def init(keyfile: str):
@@ -41,20 +44,44 @@ def insert(resource_name: str):
     with open(filename,"wb") as fobj:
         fobj.write(enc)
     print(f"Password successfully saved under {resource_name}.")
-    
+
+
+def show(resource_name: List[str]):
+    accname = resource_name[0].strip()
+    dirname = os.path.expanduser("~/.sinduk")
+    ks = jce.KeyStore(dirname)
+    key = ks.get_all_keys()[0]
+    p = pathlib.Path(dirname)
+    if not accname:
+        for fname in p.glob("**/*"):
+            if fname.is_file() and fname.name != "jce.db":
+                print(fname)
+    else:
+        print("Enter the password for Sinduk?")
+        password = getpass.getpass()
+        filepath = os.path.join(dirname,accname)
+        
+        print(filepath)
+        with open(filepath,"rb") as fobj:
+            enc = fobj.read()
+            enctext = ks.decrypt(key, enc, password)
+            text = enctext.decode("utf-8") 
+
+            print(text)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--init", help="pass the OpenPGP key file ", type=str)
     parser.add_argument("--insert", help="pass the resource name", type=str)
+    parser.add_argument("resource_name",metavar='resource_name', type=str , nargs="*", default=[""],
+                     help='Name of the account' )
     args = parser.parse_args()
     if args.init:
         init(args.init)
     elif args.insert:
         insert(args.insert)
+    else:
+        show(args.resource_name)
 
 
-
-    
-    
