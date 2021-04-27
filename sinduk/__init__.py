e42fe1bf309c84bfc9326eb5a1cd474dc43f92bc
anweshadas <anwesha@das.community>
2021-04-27 21:41:11 +0530

    Cleans the code

--- sinduk/__init__.py
+++ sinduk/__init__.py
@@ -19,6 +19,7 @@ def init(keyfile: str):
     ks = jce.KeyStore(dirname)
     ks.import_cert(keyfile)
 
+
 def insert(resource_name: str):
     "take password from the user"
     password = getpass.getpass()
@@ -26,22 +27,22 @@ def insert(resource_name: str):
     if password != password1:
         print("Both the passwords are not same. Enter the correct password.")
         return
-    
+
     dirname = os.path.expanduser("~/.sinduk")
     # encrypting the password
     ks = jce.KeyStore(dirname)
     key = ks.get_all_keys()[0]
     enc = ks.encrypt(key, password)
 
-    filename = os.path.join(dirname,resource_name)
+    filename = os.path.join(dirname, resource_name)
     # checking if the resource_name has `/` in it
     if resource_name.find("/"):
         dirname = os.path.join(dirname, os.path.dirname(resource_name))
-        # https://docs.python.org/3/library/os.html#os.makedirs 
+        # https://docs.python.org/3/library/os.html#os.makedirs
         # if the directory exists do not throw any error
         os.makedirs(dirname, 0o700, True)
-    
-    with open(filename,"wb") as fobj:
+
+    with open(filename, "wb") as fobj:
         fobj.write(enc)
     print(f"Password successfully saved under {resource_name}.")
 
@@ -59,13 +60,13 @@ def show(resource_name: List[str]):
     else:
         print("Enter the password for Sinduk?")
         password = getpass.getpass()
-        filepath = os.path.join(dirname,accname)
-        
+        filepath = os.path.join(dirname, accname)
+
         print(filepath)
-        with open(filepath,"rb") as fobj:
+        with open(filepath, "rb") as fobj:
             enc = fobj.read()
             enctext = ks.decrypt(key, enc, password)
-            text = enctext.decode("utf-8") 
+            text = enctext.decode("utf-8")
 
             print(text)
 
@@ -74,8 +75,14 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--init", help="pass the OpenPGP key file ", type=str)
     parser.add_argument("--insert", help="pass the resource name", type=str)
-    parser.add_argument("resource_name",metavar='resource_name', type=str , nargs="*", default=[""],
-                     help='Name of the account' )
+    parser.add_argument(
+        "resource_name",
+        metavar="resource_name",
+        type=str,
+        nargs="*",
+        default=[""],
+        help="Name of the account",
+    )
     args = parser.parse_args()
     if args.init:
         init(args.init)
@@ -83,5 +90,3 @@ def main():
         insert(args.insert)
     else:
         show(args.resource_name)
-
-
